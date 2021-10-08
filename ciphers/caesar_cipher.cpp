f0079183af76d1d9ccda2a1c56a51760c6c78f1d
bhantsi <bhantsi@gmail.com>
2021-10-08 17:41:37 +0100

    Update changes

--- ciphers/caesar_cipher.cpp
+++ ciphers/caesar_cipher.cpp
@@ -2,7 +2,32 @@
     Name: Bala Hantsi
     Twitter: @b_hantsi
     Github:  bhantsi
+
  */
+
+//  Entcryption
+
+   #include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int i,j,k;
+    string s,t;
+    int key;
+    cout<<"Enter the key\n";
+    cin>>key;
+    cout<<"Enter the message\n";
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        t+=(s[i]-'A'+key)%26+'A';
+    }
+    cout<<"\n\nEncrypted message is "<<t<<'\n';
+    return 0;
+}   
+
+
+// Decryption
+
 
    #include<iostream>
 #include<string>
