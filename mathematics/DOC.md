5ae4bb2d534d72ebae30b4b5b8136ebaf6da1a68
saaimsid <31106802+saaimsid@users.noreply.github.com>
2021-10-11 16:58:43 +0530

    Added Prime Factor of a number (#45)

--- mathematics/DOC.md
+++ mathematics/DOC.md
@@ -8,3 +8,15 @@
 `n! = n . (n-1) . (n-2) . (n-3) ...... 3 . 2 . 1`  
 For example,  `5! = 5 . 4 . 3 . 2 . 1 = 120`  
 The value of `0!` is `1`, according to the convention for an empty product. [[Wikipedia](https://en.wikipedia.org/wiki/Factorial)]
+
+## Prime Factors of a Number
+
+- [Source code](./prime_factors_of_n.cpp)
+
+- In mathematics, factors of a non-negative integer n, are positive integers less than n which completely divides n,i.e., they leave remainder 0 when n is divided by them. Prime factors of a number n are factors of n which are prime numbers.
+
+### For example: 
+<pre>
+    Factors of 30 are 1, 2, 3, 5, 6, 10, 15
+    Prime Factors of 30 are 2, 3, 5
+</pre>
---
+++ mathematics/prime_factors_of_n.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+void uniquePrime(int n){
+    int i;
+    vector<int> factors;
+    while(n % 2 == 0){                              //skip next 2s
+        if(find(factors.begin(),factors.end(),2) == factors.end())     //checking if factor already present in the vector
+            factors.push_back(2);
+        n = n/2;
+    }
+    for(i = 3; i <= sqrt(n); i=i+2){                //i will increment by 2, to get only odd integers
+        while(n % i == 0){                          //skip next i's
+            if(find(factors.begin(),factors.end(),i) == factors.end())     //checking if factor already present in the vector
+                factors.push_back(i);
+            n = n/i;
+        }
+    }
+    if(n > 2){                                      //if number has a prime factor greater than 2 
+        if(find(factors.begin(),factors.end(),i) == factors.end())     
+            factors.push_back(n);
+    }
+    
+    for(i=0;i<factors.size();i++){                  //Printing all the prime factors
+        cout<<factors[i]<<"  ";
+    }
+   
+}
+int main() {
+   int n;
+   cout<<"\n\tEnter a number: ";
+   cin>>n;
+   cout<<"\n\tPrime factors of "<<n<<" : ";
+   uniquePrime(n);
+   return 0;
+}
